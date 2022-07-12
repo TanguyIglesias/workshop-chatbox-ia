703a641a199c3de6d791d46dfdf8822ef390e36d
SashBFa <bastienfajs@gmail.com>
2022-07-12 14:11:00 +0200

    break word

--- src/components/chatbox/index.tsx
+++ src/components/chatbox/index.tsx
@@ -23,12 +23,23 @@ const ChatBoxContainer = () => {
       setUserMessage("");
       setCreateBox(false);
     }
-  }, [createBox]);
+  }, [createBox, textDisplay, userMessage]);
 
   return (
     <main className="border h-full p-2 rounded-lg shadow-lg flex flex-col justify-end">
       {textDisplay &&
-        textDisplay.map((message) => <p key={message.id}>{message.message}</p>)}
+        textDisplay.map((message) =>
+          message.user === "user" ? (
+            <div
+              key={message.id}
+              className="bg-blue-600 p-2 m-2 rounded-lg w-72 shadow-lg ml-auto "
+            >
+              <p className="break-words">{message.message}</p>
+            </div>
+          ) : (
+            <div key={message.id}>{message.message}</div>
+          )
+        )}
       <InputBar
         userMessage={userMessage}
         setUserMessage={setUserMessage}
